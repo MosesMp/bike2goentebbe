5ae5b8905d07bab0249a22965ca8486f31733f09
Moses Mpiriirwe <88707929+MosesMp@users.noreply.github.com>
2023-02-26 03:28:41 +0100

    Update main.js

--- main.js
+++ main.js
@@ -31,3 +31,13 @@ let startPos = 0;
 let currentTranslate = 0;
 let prevTranslate = 0;
 
+// Check if there are problems with the mobile version of the site
+const hasMobileIssues = true; // Set this to true if there are issues with the mobile version of the site
+
+// Display an alert message if there are issues with the mobile version of the site
+if (hasMobileIssues) {
+  alert('We are experiencing problems on our site. We are still setting up the site, so please use the web version if you encounter any issues. Alternatively, you can book with us through Trip Advisor. We apologize for any inconvenience.');
+}
+
+window.onload = function() {
+  alert("Dear valued customers, \n\nWe are currently experiencing some technical issues with our website as we are still under construction. We apologize for any inconvenience this may cause. To ensure that you can still book your trip with us, we recommend using our desktop version or proceeding with bookings through Trip Advisor. \n\nOur team is working diligently to resolve the issue as quickly as possible, and we appreciate your patience and understanding during this time. \n\nThank you for choosing our service, and we look forward to providing you with an exceptional travel experience. \n\nBest regards, \n[Bike2goEntebbe]");
