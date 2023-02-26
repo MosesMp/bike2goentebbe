cce8355dfffef4927a52c7fc9f1e8042ac5f1655
Moses Mpiriirwe <88707929+MosesMp@users.noreply.github.com>
2023-02-26 22:24:51 +0100

    Update main.js

--- main.js
+++ main.js
@@ -24,6 +24,7 @@ reserveButtons.forEach(button => {
 
 
 
+
 let slideIndex = 0;
 showSlides();
 
@@ -43,4 +44,21 @@ function showSlides() {
 
 
 
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
 
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("home-button").style.display = "block";
+  } else {
+    document.getElementById("home-button").style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+document.getElementById("home-button").onclick = function() {topFunction()};
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
